77168e30c280b8517eb5fb4c96bba9ef9958a863
Sam <samharden@gmail.com>
2017-08-21 15:12:25 -0400

    More content

--- main/views.py
+++ main/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request, 'main/home.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def criminal_defense(request):
+    return render(request, 'main/criminal-defense.html')
+
+def wte_page(request):
+    return render(request, 'main/wills-trusts-estates.html')
+
+def seal_crim(request):
+    return render(request, 'main/sealing-criminal-record.html')
+
+def injunctions(request):
+    return render(request, 'main/injunctions.html')
